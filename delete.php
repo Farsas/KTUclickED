f28e250d63e1dd2851afd4402947f9bb6578b0e4
mataxa69 <79715584+mataxa69@users.noreply.github.com>
2022-04-13 10:48:42 +0300

    update is ktu.us.lt

--- delete.php
+++ delete.php
@@ -1,4 +1,8 @@
 <?php
 	include("kintamieji.php");
+	echo $kintamasis
+	$del = "DELETE vartotojas, duomenys FROM vartotojas, duomenys WHERE vartotojas.vardas='$kintamasis' AND duomenys.vardas='$kintamasis';";
+	mysqli_query($con, $del);
+	
 	
 ?>
